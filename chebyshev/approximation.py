c43b0863ccc87185bae77c230909604288425314
mlazaric <marko.lazaric@gmail.com>
2018-12-21 15:53:12 +0100

    Add type hinting and docstrings

--- chebyshev/approximation.py
+++ chebyshev/approximation.py
@@ -1,31 +1,78 @@
-from sympy import Poly, plot, series
+from sympy import Poly, plot, series, Expr
 from sympy.abc import x
 from numpy import polyval
 from chebyshev.polynomial import lower_degree_to
 
+from typing import Tuple, Callable, List
+
 
 class Approximation:
-    def __init__(self, function, interval, polynomial_degree, taylor_degree=20, point=0):
+    """
+    Class for approximating sympy expressions using the Taylor series and Chebyshev polynomials.
+    """
+
+    def __init__(self,
+                 function: Expr,
+                 interval: Tuple[float, float],
+                 polynomial_degree: int,
+                 taylor_degree: int = 20,
+                 point: float = 0) -> None:
+        """
+        Creates a new Approximation of the given expression which is approximated with a polynomial of
+            degree `polynomial_degree`.
+
+        First it creates a Taylor polynomial of the given `function` with `taylor_degree` degree around
+            `point`. Then it lowers the polynomial degree using Chebyshev polynomials until the approximated
+            polynomial degree is less than or equal to `polynomial_degree`.
+
+        :param function: sympy expression to approximate
+        :param interval: interval on which to approximate (used for calculating error and plotting)
+        :param polynomial_degree: maximum degree of the approximation polynomial
+        :param taylor_degree: degree of the starting Taylor polynomial
+        :param point: point around which to create the Taylor polynomial
+        :rtype: None
+        """
+
         self.function = function
         self.interval = interval
         self.polynomial_degree = polynomial_degree
 
         self.__approximate_function(taylor_degree, point)
 
-    def __approximate_function(self, taylor_degree, point):
-        taylor_approximation = series(self.function, x, point, n=taylor_degree).removeO()
+    def __approximate_function(self,
+                               taylor_degree: int,
+                               point: float) -> None:
+        """
+        Private function for approximating the sympy expression and lowering the Taylor polynomial's degree.
+
+        :param taylor_degree: degree of the starting Taylor polynomial
+        :param point: point around which to create the Taylor polynomial
+        :rtype: None
+        """
+
+        taylor_approximation = Poly(series(self.function, x, point, n=taylor_degree).removeO())
 
         self.approximation = lower_degree_to(taylor_approximation, self.polynomial_degree)
 
-    def get_coeffs(self):
-        coeffs = []
+    @property
+    def coeffs(self) -> List[float]:
+        """
+        Function to get coefficients of the approximation polynomial.
 
-        for coeff in reversed(Poly(self.approximation).all_coeffs()):
-            coeffs.append(float(coeff))
+        :return: list of coefficients of the approximation polynomial
+        :rtype: List[float]
+        """
 
-        return coeffs
+        return [float(coeff) for coeff in self.approximation.all_coeffs()]
 
-    def print_coeffs_as_table(self):
+    def print_coeffs_as_table(self) -> None:
+        """
+        Prints coefficients of the approximation polynomial in a simple markdown table with two columns,
+            first column contains the coefficients and the second the terms.
+
+        :rtype: None
+        """
+
         print('|        Coefficient        |  Term  |')
         print('|---------------------------|--------|')
 
@@ -38,35 +85,88 @@ class Approximation:
 
             print(f'| `{fcoeff:+1.20f}` | `{xterm}` |')
 
-    def get_error(self):
+    def get_error(self,
+                  step: float = 0.01) -> float:
+        """
+        Calculates the maximum absolute error between the given function and the approximation polynomial on the
+            interval `interval` with the `step` step.
+
+        :param step: step for calculating error
+        :return: maximum error calculated using sympy on the given interval, with the given step
+        :rtype: float
+        """
+
         error = 0
         curr_x = self.interval[0]
 
-        while curr_x <= (self.interval[1] + 0.01):
+        while curr_x <= (self.interval[1] + step):
             error = max(abs((self.function - self.approximation).subs({x: curr_x})), error)
-            curr_x += 0.01
+            curr_x += step
 
         return float(error)
 
-    def get_numpy_error(self, function):
-        coeffs = self.get_coeffs()
+    def get_numpy_error(self,
+                        function: Callable,
+                        step: float = 0.01) -> float:
+        """
+        Calculates the maximum absolute error between the given function and the approximation polynomial on the
+            interval `interval` with the `step` step.
+
+        :param function: a callable function which returns the same result as `function`
+        :param step: step for calculating error
+        :return: maximum error calculated using numpy on the given interval, with the given step
+        :rtype: float
+        """
+        coeffs = self.coeffs
         error = 0
         curr_x = self.interval[0]
 
-        while curr_x <= (self.interval[1] + 0.01):
-            error = max(abs(function(curr_x) - polyval(coeffs[::-1], curr_x)), error)
-            curr_x += 0.01
+        while curr_x <= (self.interval[1] + step):
+            error = max(abs(function(curr_x) - polyval(coeffs, curr_x)), error)
+            curr_x += step
 
         return error
 
-    def plot_approximation(self):
+    def plot_approximation(self) -> None:
+        """
+        Plots the approximation polynomial using sympy's plot function on the given interval.
+
+        :rtype: None
+        """
+
         plot(self.approximation, (x, self.interval[0], self.interval[1]))
 
-    def plot_absolute_error(self, ylabel=''):
+    def plot_absolute_error(self,
+                            ylabel: str = '') -> None:
+        """
+        Plots the absolute error between the function and the approximation polynomial using sympy's plot
+            on the given interval.
+
+        :param ylabel: label for the y axis
+        :rtype: None
+        """
+
         plot(abs(self.function - self.approximation), (x, self.interval[0], self.interval[1]), ylabel=ylabel)
 
 
-def get_best_approximation(function, interval, polynomial_degree, start_taylor_degree=None, point=0):
+def get_best_approximation(function: Expr,
+                           interval: Tuple[float, float],
+                           polynomial_degree: int,
+                           start_taylor_degree: int = None,
+                           point: float = 0) -> Approximation:
+    """
+    Finds the best approximation by increasing the starting Taylor polynomial degree until the maximum absolute error
+        stops decreasing.
+
+    :param function: sympy expression to approximate
+    :param interval: interval on which to approximate (used for calculating error and plotting)
+    :param polynomial_degree: maximum degree of the approximation polynomial
+    :param start_taylor_degree: degree of the starting Taylor polynomial
+    :param point: point around which to create the Taylor polynomial
+    :return: the approximation with the smallest maximum absolute error
+    :rtype: Approximation
+    """
+
     if start_taylor_degree is None:
         start_taylor_degree = polynomial_degree + 1
 
@@ -78,6 +178,5 @@ def get_best_approximation(function, interval, polynomial_degree, start_taylor_d
         prev_approximation = curr_approximation
         curr_approximation = Approximation(function, interval, polynomial_degree, taylor_degree, point)
         taylor_degree += 1
-        print(taylor_degree)
 
     return prev_approximation
--- chebyshev/polynomial.py
+++ chebyshev/polynomial.py
@@ -1,36 +1,77 @@
+from sympy import Poly
 from sympy.abc import x
-from sympy import LC, simplify, degree
-
-computed_polynomials = [1, x]
 
 
-def get_nth_chebyshev_polynomial(polynomial_degree):
+"""
+Caching for the computed Chebyshev polynomials.
+"""
+computed_polynomials = [Poly(1, x), Poly(x, x)]
+
+
+def get_nth_chebyshev_polynomial(polynomial_degree: int) -> Poly:
+    """
+    Calculates the nth Chebyshev polynomial using the recursive relation:
+
+        T_n = 2x * T_{n - 1} - T_{n - 2}
+
+    Caches the result in the computed_polynomials list.
+
+    :param polynomial_degree: degree of the Chebyshev polynomial
+    :return: the nth Chebyshev polynomial
+    :rtype: Poly
+    """
     if (len(computed_polynomials) - 1) >= polynomial_degree:
         return computed_polynomials[polynomial_degree]
 
-    polynomial = simplify(2 * x * get_nth_chebyshev_polynomial(polynomial_degree - 1)
-                                - get_nth_chebyshev_polynomial(polynomial_degree - 2))
+    # T_n = 2x * T_{n - 1} - T_{n - 2}
+    T_n_1 = get_nth_chebyshev_polynomial(polynomial_degree - 1)
+    T_n_2 = get_nth_chebyshev_polynomial(polynomial_degree - 2)
 
-    computed_polynomials.append(polynomial)
+    T_n = 2 * x * T_n_1 - T_n_2
 
-    return polynomial
+    computed_polynomials.append(T_n)
 
-
-def normalise_polynomial(polynomial):
-    return polynomial / LC(polynomial)
-
-
-def get_normalised_nth_chebyshev_polynomial(max_polynomial_degree):
-    return normalise_polynomial(get_nth_chebyshev_polynomial(max_polynomial_degree))
+    return T_n
 
 
-def lower_degree_to(polynomial, max_polynomial_degree):
-    current_polynomial_degree = degree(polynomial)
+def normalise_polynomial(polynomial: Poly) -> Poly:
+    """
+    Normalises polynomial by dividing it by its leading coefficient.
 
-    while degree(polynomial) > max_polynomial_degree:
-        normalised_chebyshev_polynomial = get_normalised_nth_chebyshev_polynomial(current_polynomial_degree)
+    :param polynomial: polynomial to normalise
+    :return: normalised polynomial
+    :rtype: Poly
+    """
 
-        polynomial -= normalised_chebyshev_polynomial * LC(polynomial)
-        current_polynomial_degree = degree(polynomial)
+    return polynomial / polynomial.LC()
+
+
+def get_normalised_nth_chebyshev_polynomial(polynomial_degree: int) -> Poly:
+    """
+    A helper function to return the normalised nth Chebyshev polynomial.
+
+    :param polynomial_degree: degree of the Chebyshev polynomial
+    :return: normalised Chebyshev polynomial
+    :rtype: Poly
+    """
+
+    return normalise_polynomial(get_nth_chebyshev_polynomial(polynomial_degree))
+
+
+def lower_degree_to(polynomial: Poly,
+                    max_polynomial_degree: int) -> Poly:
+    """
+    Lowers the degree of the polynomial by using Chebyshev polynomials.
+
+    :param polynomial: polynomial to lower the degree of
+    :param max_polynomial_degree: maximum polynomial degree to lower to
+    :return: polynomial with the degree less than or equal to `max_polynomial_degree`
+    :rtype: Poly
+    """
+
+    while polynomial.degree() > max_polynomial_degree:
+        normalised_chebyshev_polynomial = get_normalised_nth_chebyshev_polynomial(polynomial.degree())
+
+        polynomial -= normalised_chebyshev_polynomial * polynomial.LC()
 
     return polynomial
